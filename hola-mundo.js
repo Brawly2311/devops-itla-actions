e0706edcefeb2fe952e44abc74f78c28541ecb3d
Brawly2311 <141943184+Brawly2311@users.noreply.github.com>
2025-10-04 22:53:13 -0400

    Create hola-mundo.js

---
+++ hola-mundo.js
@@ -0,0 +1,4 @@
+// Programa Hola Mundo en JavaScript
+console.log("¡Hola Mundo desde DevOps ITLA!");
+console.log("Práctica de GitHub Actions");
+console.log("Notificación enviada a ntfy.sh/devops-itla");
